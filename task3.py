3dd9018a51c7eb5d21458d6e9f2414b43d7ae9e7
mahmoud947 <65209396+mahmoud947@users.noreply.github.com>
2022-03-31 21:16:04 +0200

    just rename

--- task3.py
+++ task3.py
@@ -1,52 +1,32 @@
-from datetime import datetime
-import string
-
-phrase = "Pony stash token"
-
-print(phrase[5:10])
-print(phrase[-16:-11])
-print(phrase[::-1])
-print(phrase[0::3])
+_grade_dec = {
+    (90, 100): "A",
+    (80, 89): "B",
+    (70, 79): "C",
+    (60, 69): "D",
+    (0, 59): "F",
+}
 
 
-def get_full_name_formated(full_name_Input: str):
-    arr_full_name = full_name_Input.split()
-    full_name = ""
-    try:
-        first_name = arr_full_name[0].lower()
-        last_name = arr_full_name[1].lower()
-        full_name = first_name.capitalize()+" "+last_name.capitalize()
-        return full_name
-    except:
-        raise ValueError
+def get_grade(grade: int):
+    for (min, max) in _grade_dec:
+        if(min < grade and max > grade):
+            return _grade_dec[(min, max)]
 
 
-def dateFormated(time_str: str):
-    time = datetime.strptime(time_str, "%d%m%Y").strftime("%d/%m/%Y")
-    return str(time)
+def fizz_buzz():
+    for i in range(100):
+        if (i % 3 == 0):
+            print("FIZZ")
+            continue
+        elif (i % 5 == 0):
+            print("BUZZ")
+            continue
+        elif (i % 3 == 0) and (i % 5 == 0):
+            continue
+            print(" FIZZ BUZZ")
+        print(i)
 
 
-def get_birth_day_formated(birth_day_input: str):
-    birth_day_len = len(str(birth_day_input))
-    if birth_day_len != 8:
-        raise ValueError
-    else:
-        return dateFormated(str(birth_day_input))
-
-
-def form():
-    try:
-        full_name_Input = input("enter your full name \n")
-        full_name = get_full_name_formated(full_name_Input)
-
-        birth_day_input = input(
-            "enter your Birthday by an 8-digit like `10031999`\n")
-        birth_day = get_birth_day_formated(birth_day_input)
-
-        print(full_name+" was born on "+birth_day)
-    except:
-        print("error \n please enter your full name like `mahmoud kamal` and enter your Birthday by an 8-digit like `10031999")
-        form()
-
-
-form()
+sub = int(input("Enter marks obtained in subject : "))
+print(get_grade(sub))
+fizz_buzz()
